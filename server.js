9cb495cbae5132ef6743cb53fb563139792f5b7c
azumahjoshua <joshuaazumah1086@gmail.com>
2024-11-29 06:12:55 +0000

    set docker compose file

--- server.js
+++ server.js
@@ -4,15 +4,17 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+const appName = process.env.APP_NAME
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve the HTML file on the root route
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+  console.log(`Request served by ${appName}`);
 });
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`${appName} is listening on port ${PORT}`);
 });
